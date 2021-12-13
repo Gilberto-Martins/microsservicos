e341b6d8d2436b99fa82ff2a0f8001552a1244a9
Gilberto Martins <gilbertomartins643@gmail.com>
2021-12-13 20:29:34 -0300

    init

---
+++ microsservico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace microsservico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
